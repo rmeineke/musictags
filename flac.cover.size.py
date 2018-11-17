5147cc987df9fd52033dc1c418fa7f5a5201d1fe
Robert Meineke <rmeineke@gmail.com>
2018-11-17 01:11:53 -0800

    flac covers seem to work

--- flac.cover.size.py
+++ flac.cover.size.py
@@ -27,8 +27,8 @@ def main():
 
     oversized_covers = []
     missing_covers = []
-    # path = '/home/robertm/programming/musictags/music'
-    path = '/home/robertm/music'
+    path = '/home/robertm/programming/musictags/music'
+    # path = '/home/robertm/music'
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.lower().endswith('.flac'):
@@ -39,51 +39,23 @@ def main():
                     if len(pics) == 0:
                         missing_covers.append(f'pic not found: {f}')
                     else:
-                        print(f'{pics}')
+                        # print(f'{pics}')
                         for p in pics:
-                            # p.mime
-                            #  - or mime_type
-                            # p.desc
-                            #
-                            # picture = Picture()
-                            # picture.data = data
-                            # picture.type = 17
-                            # picture.desc = u"A bright coloured fish"
-                            # picture.mime = u"image/jpeg"
-                            # picture.width = 100
-                            # picture.height = 100
-                            # picture.depth = 24
-
-                            print(f'{p.type}')
-                            size = sys.getsizeof(p.data)
-                            print(f'{size}')
                             if p.type != 3:
                                 print(f'\n\n\n >>>>>>>>>{p.type}')
-
+                            size = sys.getsizeof(p.data)
+                            if size > 150000:
+                                oversized_covers.append(f'{size} /// {f}')
+                            elif size < 10000:
+                                # looking for the generic cover ... album.jpg
+                                print(f'{size} // this may be the placeholder // {f}')
                 except Exception as e:
+                    # still not sure what might arise here
                     print(f'{e}')
                     print(f'...................{f}')
 
                 flacfile = FLACFile(f)
-                # pic_not_found = True
-                # tags = mutagen.mp3.Open(f)
-                # for i in tags:
-                #     if i.startswith('APIC'):
-                #         pic_not_found = False
-                #         data = tags[i].data
-                #         size = sys.getsizeof(data)
-                #         if size > 150000:
-                #             oversized_covers.append(f'{size} /// {f}')
-                #         elif size < 10000:
-                #             # looking for the generic cover ... album.jpg
-                #             print(f'{size} // this may be the placeholder // {f}')
-                #         break
-                #
-                # if pic_not_found:
-                #     missing_covers.append(f'pic not found: {f}')
-                #
-                # mp3 = MP3File(f)
-
+                
     print(f'\n')
     print(f'ttl files processed: {FLACFile.ttl_files_processed}')
     print(f'ttl file size: {FLACFile.ttl_file_size:,}')
